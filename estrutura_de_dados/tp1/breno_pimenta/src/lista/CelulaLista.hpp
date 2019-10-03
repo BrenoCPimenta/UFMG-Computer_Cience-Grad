c4f8dfb1f34ec73d0eaa44e5c73a5f499db7c859
BrenoCPimenta <brenocpimenta@gmail.com>
2019-10-03 17:13:27 -0300

    Em ED, modif. notasArv e estrutura tp1

---
+++ estrutura_de_dados/tp1/breno_pimenta/src/lista/CelulaLista.hpp
@@ -0,0 +1,22 @@
+#ifndef CELULA_H
+#define CELULA_H
+
+using namespace = std;
+
+public class CelulaLista{
+  private:
+    int _valor = NULL;
+    int _proximo = NULL;
+    int _anterior = NULL;
+    
+  public:
+    CelulaLista(int, int, int);
+    void setProximo(int);
+    void setValor(int);
+    void setAnterior(int)
+    int getValor();
+    int getProximo();
+    int getAnterior();
+    
+}
+#endif 
---
+++ estrutura_de_dados/tp1/breno_pimenta/src/testes/main_test.cpp
@@ -0,0 +1,5 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+#include "../lista/CelulaLista.hpp"
+#include "../lista/Lista.hpp"
