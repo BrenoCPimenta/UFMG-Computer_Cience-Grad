5195d8d0e4eabd3f03c385aa7c84f11ab141db1e
BrenoCPimenta <brenocpimenta@gmail.com>
2020-08-16 22:24:01 -0300

    Modify test enviroment

--- algoritmos_l/tp1/project/run_tests.sh
+++ algoritmos_l/tp1/project/run_tests.sh
@@ -1,9 +1,15 @@
 EXEC=$1
 TMP_OUT=tp3.out
 
-echo "Teste:"
-for i in {1..18..1}; do
+for i in {1..2..1}; do
   testIN=$(printf "%02d" $i)
   testOUT=$(printf "%02d" $i)
-  $EXEC < tests/$testIN.in > tests/$testOUT.ou
+  $EXEC < tests/$testIN.in > $TMP_OUT 
+  if ! diff -qwB tests/$testOUT.ou $TMP_OUT &>/dev/null; then
+    echo "Test $testIN failed"
+  else
+    echo "Test $testIN  passed"
+  fi
 done
+rm $TMP_OUT
+rm tp1
