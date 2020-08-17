802362e857a081b2af2e5805d9ee288aae0e38cd
BrenoCPimenta <brenocpimenta@gmail.com>
2020-08-17 00:07:24 -0300

    Add new IN data treatment

--- algoritmos_l/tp1/project/src/main.cpp
+++ algoritmos_l/tp1/project/src/main.cpp
@@ -4,15 +4,32 @@ using namespace std;
 
 
 int main(){
-  int table_N;
-  int table_M;
-  int players;
 
-  //Lendo parametros iniciais
-  cin>>table_N>>table_M>>players;
+    //Lendo parametros iniciais
+    int table_N;
+    int table_M;
+    int players;
 
-  cout<<table_N<<endl;
-  cout<<table_M<<endl;
-  cout<<players<<endl;
+    cin>>table_N>>table_M>>players;
+
+    int table[table_N][table_M];
+    bool visited_positions[table_N][table_M];
+    int players_initial_position[players][players];
+
+    for(int n=0; n<table_N; n++) {
+        for(int m=0; m<table_M; m++) {
+            cin>>table[n][m];
+            visited_positions[n][m] = false;
+        }
+    }
+    for(int i=0; i<players; i++) {
+        cin>>players_initial_position[i][0];
+        cin>>players_initial_position[i][1];
+        visited_positions[players_initial_position[i][0]][players_initial_position[i][1]] = true;
+    }
+
+    cout<<table_N<<endl;
+    cout<<table_M<<endl;
+    cout<<players<<endl;
 
 }
