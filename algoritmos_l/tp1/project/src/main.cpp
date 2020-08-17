77f6eb75d1640b23e7060b20128b46494433bea3
BrenoCPimenta <brenocpimenta@gmail.com>
2020-08-16 22:07:25 -0300

    Create main and Makefile

---
+++ algoritmos_l/tp1/project/src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+  int table_N;
+  int table_M;
+  int players;
+
+  //Lendo parametros iniciais
+  cin>>table_N>>table_M>>players;
+
+  cout<<table_N<<endl;
+  cout<<table_M<<endl;
+  cout<<players<<endl;
+
+}
